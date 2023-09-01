d1c6b574d99a1194c8a569f89cd6ee54bbd3f860
PsyQox <pikithor@gmail.com>
2023-09-01 16:19:50 -0500

    tabla intermedia

--- server/src/controllers/User/postUser.js
+++ server/src/controllers/User/postUser.js
@@ -1,18 +1,21 @@
-const {User} = require('../../db')
+const {User,Rol} = require('../../db')
 
-const createUser = async(name,email,password,phone,privilege, businessId) =>{
-        const [newUser,created] = await User.findOrCreate({
-            where:{
-                name,
-                email,
-                password,
-                phone,
-                privilege,
-                BusinessId: businessId
-            }
+const createUser = async(name,email,password,phone,privilege,rolIdRow,businessId) =>{
+    // example for rolIdRow ["998a0c02-4ff1-434c-9108-05cd8c72b873"]
+        const [newUser,created] = await User.findOrCreate({where:{name:name},defaults:{
+            email,
+            password,
+            phone,
+            privilege,
+            BusinessId: businessId
+        }
         })
         
-        await newUser.save()
+        for(const rolId of rolIdRow){
+            const findRol = await Rol.findOne({where:{id:rolId}})
+            await newUser.addRol(findRol)
+        }
+        
         return newUser
 }
 
