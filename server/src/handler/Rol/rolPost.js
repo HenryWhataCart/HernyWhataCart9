227c2968762e2ecbaf1dea2e86818ae46f0afc0c
PsyQox <pikithor@gmail.com>
2023-08-31 21:30:49 -0500

    Resolución

--- server/src/handler/Rol/rolPost.js
+++ server/src/handler/Rol/rolPost.js
@@ -2,7 +2,8 @@ const createRol = require("../../controllers/Rol/createRol");
 
 const createRolHandler = async (req, res) => {
   try {
-    await createRol(req, res);
+    const newRol = await createRol(req, res);
+    res.status(201).json(newRol);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
