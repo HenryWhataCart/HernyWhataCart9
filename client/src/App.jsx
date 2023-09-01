53dd647e27d77e539c4a869ee43370a4606d7ff9
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-01 19:43:00 -0300

    rutas actualizadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import axios from 'axios'
 import Dashboard from './views/Dashboard/Dashboard'
-// import { AuthenticationGuard } from "./components/Auth0/AuthenticationGuard/AuthenticationGuard";
+import { AuthenticationGuard } from "./components/Auth0/AuthenticationGuard/AuthenticationGuard";
 import SignIn from './views/SignIn/SignIn'
 import SignOut from './components/Auth0/SignOut/SignOut';
-import ChatList from './components/ChatList/ChatList';
 import { Contacts } from './views/Contacts/Contacts'
+import Layout from './components/Layout/Layout'
 
 axios.defaults.baseURL = 'http://localhost:3001'
 
@@ -16,31 +16,26 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route
-          path='/'
-          element= {<SignIn />}
-        />
+       
+          <Route
+            path='/'
+            element= {<SignIn />}
+          />
 
-        <Route
-          path='/signout'
-          element= {<SignOut />}
-        />
+          <Route
+            path='/signout'
+            element= {<SignOut />}
+          />
 
-        <Route
-          path='/dashboard'
-          element= {<Dashboard />} //{<AuthenticationGuard component={Dashboard} />}
-        />
+          <Route
+            path='/dashboard'
+            element= {<AuthenticationGuard component={Dashboard} />}
+          />
 
-        <Route
-        path='/contacts'
-        element={<Contacts />}
-        />
-        
-        {/* ROUTE DE PRUEBA */}
-        <Route
-          path='/prueba'
-          element= {<ChatList />}
-        />
+          <Route
+          path='/contacts'
+          element={<Contacts />}
+          />
       </Routes>
     </div>
   )
--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material"
 import NavBar from "../../components/NavBar/NavBar"
 import styles from './Dashboard.module.css'
+// import ChatList from "../../components/ChatList/ChatList"
 
 const Dashboard = () => {
 
     return (
         <Box className={styles.dashboardContainer}>
             <NavBar />
+            {/* <ChatList /> */}
         </Box>
     )
 }
