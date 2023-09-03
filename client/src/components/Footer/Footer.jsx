e3b95f19eb31f5287017cbe9b8684ddc3fd4bcac
ValeHadad <valehadad@gmail.com>
2023-09-03 17:21:44 +0300

    footer con el boton relacionado a support

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { styled } from "@mui/system";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 
 const theme = createTheme({
   palette: {
@@ -25,6 +26,12 @@ const ResponsiveFooter = styled(Box)(({ theme }) => ({
 }));
 
 export default function Footer() {
+  const navigate = useNavigate();
+
+  const handleSupportClick = () => {
+    navigate("/support");
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <ResponsiveFooter
@@ -37,7 +44,6 @@ export default function Footer() {
       >
         <Container maxWidth="lg">
           <Grid container spacing={2}>
-            {/* Columna de botón de soporte */}
             <Grid
               item
               xs={12}
@@ -53,11 +59,14 @@ export default function Footer() {
                 },
               }}
             >
-              <Button variant="contained" color="customGreen">
+              <Button
+                variant="contained"
+                color="customGreen"
+                onClick={handleSupportClick}
+              >
                 Support
               </Button>
             </Grid>
-            {/* Columna de texto de copyright */}
             <Grid
               item
               xs={12}
@@ -72,9 +81,9 @@ export default function Footer() {
             >
               <Typography variant="body2" color="text.primary">
                 {"Copyright © "}
-                <Link color="inherit" href="https://whatacart.ai/">
+                <MuiLink color="inherit" href="https://whatacart.ai/">
                   Whatacart
-                </Link>{" "}
+                </MuiLink>{" "}
                 {new Date().getFullYear()}
               </Typography>
             </Grid>
