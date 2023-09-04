8d15f6ab751545b9a8c5825294d51c809f4a41e0
NicoleCastiglione <nicolecastiglione0@gmail.com>
2023-09-04 02:16:11 -0300

    Business Form Registration

---
+++ client/src/components/Form Business/BusinessRegistration.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { validate } from './Validate';
+import { TextField, Button, Typography, Box } from '@mui/material';
+
+import styles from './BusinessRegistration.module.css'
+
+export const BusinessRegistration = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+    setErrors(
+      validate({
+        ...formData,
+        [event.target.name]: event.target.value,
+      })
+    );
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const error = validate(formData);
+    setErrors(error)
+   
+    // if (Object.values(error).length === 0)
+    //   navigate("/business")
+    };
+
+  const buttonStyles = {
+    backgroundColor: "#09E6A7",
+    color: "white",     
+  };
+
+  return (
+    <Box display="flex">
+        <form onSubmit={handleSubmit} className={styles.container}>
+          <Box display="flex"><Typography variant="h4" align="center" style={{ color: "#4E4E4E" }} fontWeight={500} fontSize="25px" >
+              ¡Welcome Business!</Typography>
+          </Box>
+          <TextField
+            label="Business Name"
+            variant="outlined"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            helperText={errors.name && <p>{errors.name}</p>}
+            error={errors.name && <p>{errors.name}</p>}
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            helperText={errors.email && <p>{errors.email}</p>}
+            error={errors.email && <p>{errors.email}</p>}
+          />
+          <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            helperText={errors.password && <p>{errors.password}</p>}
+            error={errors.password && <p>{errors.password}</p>}
+          />
+          <TextField
+            label="Repeat Password"
+            variant="outlined"
+            type="password"
+            name="repeatpassword"
+            value={formData.repeatpassword}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            helperText={errors.repeatPassword && <p>{errors.repeatPassword}</p>}
+            error={errors.repeatPassword && <p>{errors.repeatPassword}</p>}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            style={buttonStyles}
+          >
+            Register
+          </Button>
+        </form>
+        </Box>
+  );
+};
