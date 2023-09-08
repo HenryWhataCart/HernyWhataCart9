a9073730af01ae088a711da625210c57c8170e00
PsyQox <pikithor@gmail.com>
2023-09-08 10:11:29 -0500

    ea

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } =require('./src/db.js')
 
-  server.listen(3001, async () => {
+  server.listen(3001, async () => { 
     await conn.sync({force:false}) 
     console.log('%s listening at 3001')
   })
---
+++ server/src/models/MsgReceived.js
@@ -0,0 +1,24 @@
+const {DataTypes} = require('sequelize')
+
+module.exports = (sequelize)=>{
+    sequelize.define('MsgReceived',{
+        id:{
+            type:DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        name:{
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        phone:{
+            type:DataTypes.BIGINT,
+            allowNull: false
+        },
+        payload:{
+            type: DataTypes.JSON,
+            allowNull:false
+        }
+    }, {timestamps: false})
+}
