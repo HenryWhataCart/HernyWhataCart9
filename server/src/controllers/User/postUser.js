b5451f77c8626e1231fa90ef3fb71ca08e07ca59
Matías Cuadra <MatiasCuadra98>
2023-09-08 19:57:00 -0300

    user put test

--- server/src/controllers/User/postUser.js
+++ server/src/controllers/User/postUser.js
@@ -1,22 +1,31 @@
-const {User,Rol} = require('../../db')
+const { User, Rol } = require("../../db");
 
-const createUser = async(name,email,password,phone,privilege,rolIdRow, businessId) =>{
-    // example for rolIdRow ["998a0c02-4ff1-434c-9108-05cd8c72b873"]
-        const [newUser,created] = await User.findOrCreate({where:{name:name},defaults:{
-            email,
-            password,
-            phone,
-            privilege,
-            BusinessId: businessId
-        }
-        })
-        
-        for(const rolId of rolIdRow){
-            const findRol = await Rol.findOne({where:{id:rolId}})
-            await newUser.addRol(findRol)
-        }
+const createUser = async (
+  name,
+  email,
+  password,
+  phone,
+  privilege,
+  rolIdRow,
+  businessId
+) => {
+  const [newUser, created] = await User.findOrCreate({
+    where: { name: name },
+    defaults: {
+      email,
+      password,
+      phone,
+      privilege,
+      BusinessId: businessId,
+    },
+  });
 
-        return newUser
-}
+  for (const rolId of rolIdRow) {
+    const findRol = await Rol.findOne({ where: { id: rolId } });
+    await newUser.addRol(findRol);
+  }
 
-module.exports = createUser
+  return newUser;
+};
+
+module.exports = createUser;
