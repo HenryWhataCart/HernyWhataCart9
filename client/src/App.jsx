3c811a44403bd8672e63ca3d4c5b9dd3687867cb
Ivan <carivan14@homtail.com>
2023-09-11 08:54:58 -0300

    commit for view changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
 /* eslint-disable no-unused-vars */
-import FormCreateMember from '../src/components/Forms/FormCreateMember/FormCreateMember';
+
 import './App.css'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import axios from 'axios'
-import Dashboard from './views/Dashboard/Dashboard'
+
+import { Route, Routes, useLocation } from 'react-router-dom'
+
 import { AuthenticationGuard } from "./components/Auth0/AuthenticationGuard/AuthenticationGuard";
+import { Contacts } from './views/Contacts/Contacts'
+import { CreateBusiness } from './components/Forms/FormCreateBusiness/BusinessRegistration';
+import Dashboard from './views/Dashboard/Dashboard'
+import Error from './views/Error/Error'
+import Footer from './components/Footer/Footer';
+import FormCreateMember from '../src/components/Forms/FormCreateMember/FormCreateMember';
+import Metricas from './views/Metricas/Metricas';
+import NavBar from './components/NavBar/NavBar';
+import NewSuperAdmin from './views/NewSuperAdmin/NewSuperAdmin';
 import SignIn from './views/SignIn/SignIn'
 import SignOut from './components/Auth0/SignOut/SignOut';
-import { Contacts } from './views/Contacts/Contacts'
-import Error from './views/Error/Error'
-import NavBar from './components/NavBar/NavBar';
-import Metricas from './views/Metricas/Metricas';
-import NewSuperAdmin from './views/NewSuperAdmin/NewSuperAdmin';
-import Footer from './components/Footer/Footer';
-import Support from './components/Support/Support';
-import { CreateBusiness } from './components/Forms/FormCreateBusiness/BusinessRegistration';
 import SuperAdmin from './views/SuperAdmin/SuperAdmin';
-import FormCreateRol from './components/Forms/FormCreateRol/FormCreateRol'
-
+import Support from './components/Support/Support';
+import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3001'
 
@@ -85,12 +86,6 @@ function App() {
           path='/createbusiness'
           // element= {<AuthenticationGuard component={CreateBusiness} />}
           element= {<CreateBusiness />}
-        />
-
-        <Route
-          path='/createRol/:businessId/:businessName'
-          // element= {<AuthenticationGuard component={FormCreateRol} />}
-          element= {<FormCreateRol />}
         />
 
         <Route
