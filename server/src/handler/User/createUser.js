d80bb1805f2323a06d5ca48e9ec07e2da13d985c
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-11 00:09:27 -0300

    update

--- server/src/handler/User/createUser.js
+++ server/src/handler/User/createUser.js
@@ -1,32 +1,5 @@
 const createUser = require("../../controllers/User/postUser");
 
-<<<<<<< HEAD
-const userPost = async (req, res) => {
-  const { name, email, password, phone, privilege, rolIdRow, businessId } =
-    req.body;
-  // console.log({name,email,password, phone,privilege, rolIdRow});
-  try {
-    if (!name || !email || !password || !phone)
-      return res.status(404).json({ error: "required data not found" });
-    const newUser = await createUser(
-      name,
-      email,
-      password,
-      phone,
-      privilege,
-      rolIdRow,
-      businessId
-    );
-    if (!newUser)
-      return res
-        .status(404)
-        .json({ error: "an error occurred while creating user" });
-    return res.status(200).json(newUser);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
-=======
 const userPost = async(req,res) =>{
     const {name,email,password, phone, privilege, businessId} = req.body
     try {
@@ -38,6 +11,5 @@ const userPost = async(req,res) =>{
         return res.status(500).json({error: error.message})
     }
 }
->>>>>>> 80964560514f04efa09ad3be8b3f6130999e5994
 
 module.exports = userPost;
