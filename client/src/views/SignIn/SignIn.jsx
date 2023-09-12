b61b2bac64a425ae965643716b3bcef777627a10
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-12 20:09:37 -0300

    sasa

--- client/src/views/SignIn/SignIn.jsx
+++ client/src/views/SignIn/SignIn.jsx
@@ -4,18 +4,18 @@ import { useAuth0 } from "@auth0/auth0-react"
 
 const SignIn = () => {
 
-  // const { loginWithRedirect } = useAuth0()
+  const { loginWithRedirect } = useAuth0()
   const {user} = useAuth0()
   const [loginData, setLoginData] = useState({})
-  // const [redirectUrl, setRedirectUrl] = useState('/')
+  const [redirectUrl, setRedirectUrl] = useState('/dashboard')
 
-  // const login = async () => {
-  //   await loginWithRedirect({
-  //     appState: {
-  //     returnTo: redirectUrl,
-  //     },
-  //   })
-  // }
+  const login = async () => {
+    await loginWithRedirect({
+      appState: {
+      returnTo: redirectUrl,
+      },
+    })
+  }
 
   // const redirect = (data) => {
   //   if (data.metadata.privilege === 'SuperAdmin') return '/superadmin'
@@ -25,7 +25,7 @@ const SignIn = () => {
   useEffect(() => {
     if (user) {
       setLoginData(user['loginData'])
-      console.log(loginData)
+      console.log(loginData, 'vengo de auth0')
       // if (loginData && loginData.metadata) {
       //   setRedirectUrl(redirect(loginData))
       // }
@@ -36,8 +36,8 @@ const SignIn = () => {
     
       localStorage.setItem('loginData', JSON.stringify(loginData))
       const storedLoginData = JSON.parse(localStorage.getItem('loginData'))
-      console.log(storedLoginData);
-      // login()
+      console.log(storedLoginData, 'vengo del localStorage');
+      login()
 
   }, [])
 }
