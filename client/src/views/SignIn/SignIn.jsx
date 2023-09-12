403e91fa46ac3120d4e5c98fbf36564a9d30044b
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-12 18:35:38 -0300

    asa

--- client/src/views/SignIn/SignIn.jsx
+++ client/src/views/SignIn/SignIn.jsx
@@ -1,21 +1,38 @@
-import { useEffect } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
 import { useAuth0 } from "@auth0/auth0-react"
 
 const SignIn = () => {
 
   const { loginWithRedirect } = useAuth0()
+  const {user} = useAuth0()
+  const [loginData, setLoginData] = useState({})
+  const [redirectUrl, setRedirectUrl] = useState('/')
 
   const login = async () => {
     await loginWithRedirect({
       appState: {
-      returnTo: "/welcome",
+      returnTo: redirectUrl,
       },
     })
   }
 
+  const redirect = (data) => {
+    if (data.metadata.privilege === 'SuperAdmin') return '/superadmin'
+    else return '/dashboard'
+  }
+
   useEffect(() => {
+    if (user) {
+      setLoginData(user['loginData'])
+      console.log(loginData)
+      setRedirectUrl(redirect(loginData))
+    }
+
+    localStorage.setItem('loginData', JSON.stringify(loginData))
+    
     login()
-  }, )
+  }, [user, loginData])
 }
 
 export default SignIn 
