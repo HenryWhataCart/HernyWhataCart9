668d6ef8f33b4fcd435942d1a0b75e61edfc7eef
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-12 16:44:38 -0300

    sasa

--- client/src/views/SuperAdmin/Transition.jsx
+++ client/src/views/SuperAdmin/Transition.jsx
@@ -1,19 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useAuth0 } from "@auth0/auth0-react"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 const Transition = () => {
 
-    
+
     const {user} = useAuth0()
     const [loginData, setLoginData] = useState(null)
     const navigate = useNavigate()
+
+    const navigateTo = (ruta) => {  
+        navigate(ruta)
+    } 
+
 
     useEffect(() => {
 
         if (user) {
             setLoginData(user['loginData'])
-            loginData.metadata.privilege !== "SuperAdmin" && navigate(`/dashboard/${businessId}/${businessName}`)
+            if (loginData.metadata.privilege !== "SuperAdmin") navigateTo(`/dashboard`) //${loginData.metadata.businessId}/${loginData.metadata.businessName}
+            else navigateTo('/superadmin')
         }
 
         return () => {
