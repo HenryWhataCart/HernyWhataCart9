1d812a7275a956f5760d1cfd822af46d626fbf98
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-13 02:13:46 -0300

    asa

--- client/src/views/SuperAdmin/Redirect.jsx
+++ client/src/views/SuperAdmin/Redirect.jsx
@@ -1,32 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useAuth0 } from "@auth0/auth0-react"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
 const Redirect = () => {
 
     const { user } = useAuth0()
-    const [loginData, setLoginData] = useState(null)
     const navigate = useNavigate()
+    
 
-    
-    
-    // const getLoginData = () => {
-    //     setLoginData(user['loginData'])
-    //     loginData && console.log(loginData);
-    //     loginData && localStorage.setItem('loginData', JSON.stringify(loginData))
-    // }
-    // const conNavigate = () => {
-    //     if (loginData?.metadata?.privilege === "SuperAdmin") navigate('/superadmin')
-    //     else navigate('/dashboard') // ${loginData.metadata.businessId}/${loginData.metadata.businessName}
-    // }
+    const navigator = () => {
+        if (user?.loginData?.metadata?.privilege === "SuperAdmin") navigate('/superadmin')
+        else navigate('/dashboard') // ${loginData.metadata.businessId}/${loginData.metadata.businessName}
+    }
 
 
     useEffect(() => {
         if (user) {
-            setLoginData(user['loginData'])
-            console.log(user);
-            // console.log(loginData, "soy el estado local");
+            localStorage.setItem('loginData', JSON.stringify(user.loginData))
+            navigator()
         }
     }, [user])
 
