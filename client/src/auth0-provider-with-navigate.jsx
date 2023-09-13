c80ffa166e3151b920094a7826b92bdaa81ee995
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-13 14:20:34 -0300

    sa

--- client/src/auth0-provider-with-navigate.jsx
+++ client/src/auth0-provider-with-navigate.jsx
@@ -7,7 +7,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
 
   const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
   const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID
-  const redirectUri = import.meta.env.VITE_APP_AUTH0_CALLBACK_URL;
+  const redirectUri = import.meta.env.VITE_APP_AUTH0_CALLBACK_URL
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname)
