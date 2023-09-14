8e54fd12054501a39f148aa71bc97d4f53bb4816
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-13 21:08:56 -0300

    update

--- client/src/components/Auth0/SignOut/SignOut.jsx
+++ client/src/components/Auth0/SignOut/SignOut.jsx
@@ -10,7 +10,7 @@ const SignOut = () => {
   const signout = () => {
     logout({
       logoutParams: {
-        returnTo: "http://localhost:5173",
+        returnTo: "https://whatacart-client.vercel.app",
       },
     })
   }
