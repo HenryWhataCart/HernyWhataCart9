1cffcfd18f228c849a20146c4a076afa058c9d02
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-16 12:06:13 -0300

    borrando database de render

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 const {PORT} = process.env
 
   server.listen(PORT, async () => { 
-    await conn.sync({force:false}) 
+    await conn.sync({force:true}) 
     console.log('%s listening at 3001')
   })
