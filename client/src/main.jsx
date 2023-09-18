d80d847396c49453da2a4c23addc886a148e44fa
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-18 03:31:07 -0300

    integration update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import store from "./redux/store.js";
 
-axios.defaults.baseURL = "https://whatacart-server.onrender.com";
+axios.defaults.baseURL = "https://whatacart-backend.onrender.com/"
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
