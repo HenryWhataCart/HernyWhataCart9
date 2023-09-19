ae73681faf75f74e60b220bf5b9add60c82b69b1
juannpz <juannicolaspuchetazavala@gmail.com>
2023-09-19 17:37:24 -0300

    update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const { conn } =require('./src/db.js')
 const {PORT} = process.env
 
   server.listen(PORT, async () => {
-    await conn.sync({force:false}) 
+    await conn.sync({force:true}) 
     console.log(`%s listening at ${PORT}`)
   })
